927534915450eabeee3b1924be554d3a353cabda
PREM KUMAR <premenaga@gmail.com>
2022-05-14 10:54:10 +0530

    initial commit

---
+++ employee/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Container({ data }) {
+  console.log(data)
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-black">Container</h1>
+    </div>
+  )
+}
+
+export default Container
---
+++ employee/typings.d.ts
@@ -0,0 +1,20 @@
+export interface Employee {
+  name: Text
+  fatherName: Text
+  DOB: Date
+  gender: Text
+  maritalStatus: Text
+  identificationMark: Text
+  bloodGroup: Text
+  presentAdress: Text
+  permanentAdress: Text
+  phoneNo: Number
+  mobileNumber: Number
+  email: Text
+  emergencyContact: Number
+  initialDesignation: Text
+  department: Text
+  dateOfAppointment: Date
+  dateOfJoining: Date
+  JobType: Text
+}
